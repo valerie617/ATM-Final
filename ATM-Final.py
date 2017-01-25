d0a7fd0d75db297a82373b6a92e78319d5400223
PDHS lab <pdhslab@DSUSD.K12.CA.US>
2017-01-25 09:05:41 -0800

    minor adjustments

--- ATM-Final.py
+++ ATM-Final.py
@@ -2,16 +2,8 @@
 from Tkinter import * #gives us access to everything in the Tkinter class
 import tkMessageBox
 from PIL import Image, ImageTk
-from datetime import datetime
 from threading import *
 from math import *
-
-d= datetime.now()
-y = d.year
-h = d.hour
-
-def process():
-    print blah
 
 def retrieve():
     entrytxt = entry1.get()
@@ -25,20 +17,15 @@ def retrieve():
     global name
     label3.config(text=name)
     balance = doc.readline()
-    #balance = balance[0:-1]
     global balance
     label5.config(text=balance)
     doc.close()
     entry1.config(state=DISABLED)
 
-#def insert():
-    #label3.insert(name)
-
 def withdraw_submit():
     entrytxt2 = entry2.get()
     if float(balance) >= float(entrytxt2):
         new_balance = float(balance) - float(entrytxt2)
-        print new_balance
         balance = new_balance
         global balance
         label5.config(text=balance)
@@ -53,12 +40,9 @@ def withdraw_submit():
         tkMessageBox.showwarning("Error", "Insufficient Funds") 
         entry2.delete(0,END)       
     
-    
-    
 def deposit_submit():
     entrytxt3 = entry3.get()
     new_balance = float(balance) + float(entrytxt3)
-    print new_balance
     balance = new_balance
     global balance
     label5.config(text=balance)
@@ -80,7 +64,6 @@ def transfer_submit():
     name2 = name2[0:-1]
     balance2 = doc2.readline()
     doc2.close()
-     
     entrytxt5 = entry5.get()
     new_balance = float(balance) - float(entrytxt5)
     new_balance2 = float(balance2) + float(entrytxt5)
@@ -100,10 +83,7 @@ def transfer_submit():
     entry4.delete(0,END)
     entry5.delete(0,END)
     tkMessageBox.showinfo("Transfer", "Transaction Successful")
-    print account_number2
-    print name2
-    print new_balance2
-    
+   
 def logout():
     entry1.config(state=NORMAL)
     label3.config(text= "")
@@ -125,15 +105,11 @@ root.title("Fab Things Banking")
 entry1 = Entry(root)
 entry1.grid(row=0, column=1)
 
-
 label1 = Label(root, text="Account number:")
 label1.grid(row=0, column=0)
 
-
 button1 = Button(root, text="Submit", command=retrieve)
 button1.grid(row=0, column=3)
-
-
 
 label2 = Label(root, text="Name:")
 label2.grid(row=1, column=0, sticky=W)
@@ -152,7 +128,6 @@ label6.grid(row=3, column=0, sticky=W)
 
 entry2 = Entry(root)
 entry2.grid(row=3, column=1, sticky=W)
-entry2.bind("<Return>", process)
 
 button2 = Button(root, text=">", command=withdraw_submit)
 button2.grid(row=3, column=2, sticky=W)
@@ -162,7 +137,6 @@ label7.grid(row=4, column=0, sticky=W)
 
 entry3 = Entry(root)
 entry3.grid(row=4, column=1, sticky=W)
-entry3.bind("<Return>", process)
 
 button3 = Button(root, text=">", command=deposit_submit)
 button3.grid(row=4, column=2, sticky=W)
@@ -175,15 +149,12 @@ label9.grid(row=6, column=0, sticky=W)
 
 entry4 = Entry(root)
 entry4.grid(row=6, column=1, sticky=W)
-entry4.bind("<Return>", process)
-
 
 label10 = Label(root, text="     Amount: $")
 label10.grid(row=7, column=0, sticky=W)
 
 entry5 = Entry(root)
 entry5.grid(row=7, column=1, sticky=W)
-entry5.bind("<Return>", process)
 
 button5 = Button(root, text=">", command=transfer_submit)
 button5.grid(row=7, column=2, sticky=W)
@@ -193,14 +164,8 @@ filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="Help", command=help)
 filemenu.add_separator()
 filemenu.add_command(label="Logout", command=logout)
-
 menubar.add_cascade(label="Options", menu=filemenu)
-
 root.config(menu=menubar)
-
-
-
-
 
 
 
